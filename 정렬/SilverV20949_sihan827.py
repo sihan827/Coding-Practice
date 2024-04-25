edee97ea7ff2a60e865926bb3bfe4656670c87a8
sihan827 <sihan827@naver.com>
2024-04-25 13:52:33 +0900

    PyPy3/148ms/110696KB

---
+++ 정렬/SilverV20949_sihan827.py
@@ -0,0 +1,17 @@
+import heapq
+import math
+import sys
+
+def check_ppi(w, h):
+    return math.sqrt(w ** 2 + h ** 2) / 77
+
+N = int(sys.stdin.readline())
+hq = []
+
+for i in range(1, N+1):
+    width, height = map(int, sys.stdin.readline().split())
+    heapq.heappush(hq, (-check_ppi(width, height), i))
+
+while hq:
+    _, num = heapq.heappop(hq)
+    print(num)
